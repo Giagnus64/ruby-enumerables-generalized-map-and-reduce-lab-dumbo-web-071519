8aa0a83ce6955f630f6888f8b39477bd90d7729c
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-06-11 21:56:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,6 +11,7 @@ def reduce(source_array, starting_point = 0)
   running_total = starting_point
   source_array.length.times do |index|
     running_total = yield(source_array[index], running_total)
+    print running_total
   end
   return running_total
 end
